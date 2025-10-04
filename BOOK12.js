76b7e1c95e189da4f59b5e926729488d823620b9
suraj3869 <surajraut3869@gmail.com>
2025-10-04 19:49:00 +0530

    Create BOOK12.js

---
+++ BOOK12.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: String,
+  description: String,
+  genre: String,
+  year: Number,
+  addedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
